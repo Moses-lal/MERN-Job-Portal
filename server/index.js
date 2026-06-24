74c782fa894d11d252596bb2e9174f001cabde7e
moses-lal <moseslal2692@gmail.com>
2026-06-24 13:35:30 +0530

    cors updated

--- server/index.js
+++ server/index.js
@@ -13,7 +13,11 @@ import cloudinary from "./src/config/cloudinary.js"
 const app = express();
 
 
-app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173", credentials: true }));
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
